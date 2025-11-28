83fb0807832911cf24d05534a9d99b5cc4252887
Mathias Magnusson <mathias@magnusson.space>
2025-11-28 13:33:52 +0100

    Allow to use external llvm

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     // const target = b.standardTargetOptions(.{});
     // const optimize = b.standardOptimizeOption(.{});
+    const llvm_path = b.option([]const u8, "llvm-path", "The path to llvm to link to and use binaries from. Omit to fetch in build script.") orelse null;
 
     // TODO: make it possible to instead specify which passes to include on the command line
     var passes = std.ArrayList([]const u8).empty;
@@ -23,8 +24,6 @@ pub fn build(b: *std.Build) void {
     for (passes.items) |pass| {
         run_generate_main.addFileArg(b.path(b.fmt("passes/{s}.cc", .{pass})));
     }
-
-    const llvm = b.dependency("llvm", .{});
 
     const input_file = if (b.args != null and b.args.?.len > 0) b.args.?[0] else "you must specify an input path for the run step";
 
@@ -51,25 +50,25 @@ pub fn build(b: *std.Build) void {
     // }
     // b.installArtifact(plugin);
     const plugin = std.Build.Step.Run.create(b, "diversification");
-    plugin.addFileArg(llvm.path("bin/clang++"));
+    plugin.addFileArg(llvm(b, llvm_path, "bin/clang++"));
     plugin.addFileArg(root);
     for (passes.items) |pass| {
         plugin.addFileArg(b.path(b.fmt("passes/{s}.cc", .{pass})));
     }
-    plugin.addPrefixedDirectoryArg("-I", llvm.path("include"));
+    plugin.addPrefixedDirectoryArg("-I", llvm(b, llvm_path, "include"));
     plugin.addArgs(&.{ "-g", "-shared", "-fPIC", "-o" });
     const plugin_so = plugin.addOutputFileArg("libdiversification.so");
 
     b.getInstallStep().dependOn(&b.addInstallFile(plugin_so, "libdiversification.so").step);
 
     const clang1 = std.Build.Step.Run.create(b, "clang1");
-    clang1.addFileArg(llvm.path("bin/clang-21"));
+    clang1.addFileArg(llvm(b, llvm_path, "bin/clang"));
     clang1.addArgs(&.{ "-O0", "-emit-llvm", "-c", "-o" });
     const orig_bc = clang1.addOutputFileArg("orig.bc");
     clang1.addFileArg(.{ .cwd_relative = input_file });
 
     const opt = std.Build.Step.Run.create(b, "opt");
-    opt.addFileArg(llvm.path("bin/opt"));
+    opt.addFileArg(llvm(b, llvm_path, "bin/opt"));
     opt.addArg("-load-pass-plugin");
     opt.addFileArg(plugin_so);
     var passes_flag: []const u8 = "-passes=";
@@ -86,11 +85,25 @@ pub fn build(b: *std.Build) void {
     opt.addFileArg(orig_bc);
 
     const clang2 = std.Build.Step.Run.create(b, "clang2");
-    clang2.addFileArg(llvm.path("bin/clang-21"));
+    clang2.addFileArg(llvm(b, llvm_path, "bin/clang"));
     clang2.addArg("-o");
     const transformed_elf = clang2.addOutputFileArg("transformed.elf");
     clang2.addFileArg(transformed_bc);
 
     const run_step = b.step("run", "Run the transformation passes");
-    run_step.dependOn(&b.addInstallFile(transformed_elf, b.fmt("{s}.transformed.elf", .{std.fs.path.stem(input_file)})).step);
+    run_step.dependOn(&b.addInstallFile(orig_bc, b.fmt("{s}.orig.bc", .{std.fs.path.stem(input_file)})).step);
+    run_step.dependOn(&b.addInstallFile(transformed_bc, b.fmt("{s}.transformed.bc", .{std.fs.path.stem(input_file)})).step);
+    run_step.dependOn(&b.addInstallFile(transformed_elf, b.fmt("{s}.elf", .{std.fs.path.stem(input_file)})).step);
+}
+
+fn llvm(b: *std.Build, llvm_path: ?[]const u8, sub_path: []const u8) std.Build.LazyPath {
+    if (llvm_path) |path| {
+        return .{ .cwd_relative = b.fmt("{s}/{s}", .{ path, sub_path }) };
+    } else {
+        if (b.lazyDependency("llvm", .{})) |llvm_dep| {
+            return llvm_dep.path(sub_path);
+        } else {
+            return .{ .cwd_relative = "" };
+        }
+    }
 }
