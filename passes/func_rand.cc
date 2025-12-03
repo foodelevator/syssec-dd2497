76d0bf2b4f43cf1f49658a933293902728e859e6
grin <grin@kth.se>
2025-12-03 13:31:17 +0100

    Insertion done

--- passes/func_rand.cc
+++ passes/func_rand.cc
@@ -22,4 +22,27 @@ bool func_rand_pass(Module &M, std::mt19937_64 &gen) {
     // Create alias/reference to the module's function list 
     // By changing FL, we change the order :)
     auto &FL = M.getFunctionList();
+    Function *Prev = nullptr;
+
+    // Insert F from shuffled Funcs into FL
+    for (Function &F : Funcs) {
+        auto FuncIt = F->getIterator(); // Function to be inserted
+        if(!Prev) { // Case for the first function in Funcs
+            auto InsertPos = FL.begin(); // Insert at the beginning
+            FL.splice(InsertPos, FL, FuncIt); // Move F (first shuffled func) to the front
+            Prev = F;
+        } else { // Case for the other functions
+            auto PrevIt = Prev->getIterator(); // Iterator to the previous function in M
+            // new insert position is after the previous one
+            prevIt++; 
+            auto InsertPos = PrevIt;
+            FL.splice(InsertPos, FL, FuncIt); // Insert F at position
+            prev = F;
+        }
+    }
+
+
+
+        
+}
 
